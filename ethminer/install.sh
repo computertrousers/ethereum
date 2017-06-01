bf9256911a15a981b68638adc927eb6446866c9d
Rich <rich@computertrousers.co.uk>
2017-06-01 11:22:55 +0000

    Add new user in install script

--- ethminer/install.sh
+++ ethminer/install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+set -e
+
+_BASENAME=$(basename $0)
 _DIRNAME=$(dirname $0)
+
+[[ $EUID -ne 0 ]] && echo "[${_BASENAME}] ERROR: Only root can execute this script" && exit 1
+
+[ $(getent group ether) ] || addgroup ether -gid 1111
+[ $(getent passwd ethminer) ] || useradd -b /home -m -N -g ether -u 2222 ethminer
+
+chown ethminer:ether ${_DIRNAME}
+chown ethminer:ether ${_DIRNAME}/*
+chmod 2755 ${_DIRNAME}
+
+chown root:root $0
+chmod 540 $0
+
 add-apt-repository -y ppa:ethereum/ethereum-qt
 add-apt-repository -y ppa:ethereum/ethereum
 apt-get update
