e1037d637a6a004c91e3d1e4bbc31831811ea5df
Rich <rich@computertrousers.co.uk>
2018-03-30 15:04:48 +0000

    turbo.sh updates

--- ethminer/turbo.sh
+++ ethminer/turbo.sh
@@ -2,10 +2,12 @@
 _DIRNAME=$(dirname $0)
 _BASENAME=$(basename $0)
 
-[ ! -O "${0}" ] && echo "[${_BASENAME}] ERROR: Only owner of this script can execute it" && exit 1
+[[ $# -lt 2 ]] && echo "[${_BASENAME}] USAGE: $0 <pid> <logfile> [<cpulimit>]" && exit 1
 
-[[ $# -lt 2 ]] && echo "[${_BASENAME}] USAGE: $0 <pid> <logfile>" && exit 1
+_CPULIMIT=$(($(cat /proc/cpuinfo | grep "^processor" | tail -5 | head -2 | tail -1 | sed -e "s#proc.*:.##g") *100 + 80))
+[ "${3}" != "" ] && _CPULIMIT=${3}
+echo "_CPULIMIT=[$_CPULIMIT]"
 
-nohup tail -f ${2} 2>&1 | grep --line-buffered "Pooled new future transaction" | while read line ; do timeout 20s cpulimit -l 150 -p ${1} ; done &
+nohup tail -f ${2} 2>&1 | grep --line-buffered "Pooled new future transaction" | while read line ; do timeout 20s cpulimit -l $_CPULIMIT -p ${1} ; done &
 
 exit 0
